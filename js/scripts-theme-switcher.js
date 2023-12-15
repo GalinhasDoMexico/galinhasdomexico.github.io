4b58bd9da078455a063d9e5665012f0e27ee38ce
GalinhasDoMexico <47695682+GalinhasDoMexico@users.noreply.github.com>
2023-12-15 00:06:22 -0300

    Website Update: Light & Dark Theme
    Added new light and dark theme;
    Added new images.

---
+++ js/scripts-theme-switcher.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+   const themeButton = document.getElementById('theme-button');
+   const themeStyle = document.getElementById('theme-style');
+ 
+   themeButton.addEventListener('click', function() {
+     const body = document.body;
+     body.classList.toggle('light-mode');
+ 
+     const isLightMode = body.classList.contains('light-mode');
+     localStorage.setItem('theme', isLightMode ? 'light' : 'dark');
+ 
+     applyTheme();
+   });
+ 
+   function applyTheme() {
+     const savedTheme = localStorage.getItem('theme');
+     if (savedTheme === 'light') {
+       themeStyle.href = '../css/scriptslight-style.css';
+       document.getElementById('theme-icon').src = '../img/smoke.svg';
+     } else {
+       themeStyle.href = '../css/scripts-style.css';
+       document.getElementById('theme-icon').src = '../img/flashbang.svg';
+     }
+   }
+ 
+   // Aplicar o tema quando a página carrega
+   applyTheme();
+ });
+ 
